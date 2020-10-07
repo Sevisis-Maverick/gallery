972c4a874823d413f99b8ab6105ce2fc8c9d5007
Sevisis Maverick <Mavericksevisis@gmail.com>
2020-10-06 20:36:07 -0500

    conditional render needs to be fixed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,14 @@ import axios from 'axios';
 import React, { Component, Fragment } from 'react';
 import './App.css';
 
+import {
+  HashRouter as Router,
+  Switch,
+  NavLink,
+  Route
+} from "react-router-dom";
+
+import About from './About/About'
 import GalleryList from './GalleryList/GalleryList.js';
 
 class App extends Component {
@@ -52,15 +60,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <Router>
+          <ul className="navBar">
+            <li><NavLink to="/" exact={true}>Home</NavLink></li>
+            <li><NavLink to="About">About</NavLink></li>
+          </ul>
+          <Switch>
+            <Route path="/About">
+              <About />
+            </Route>
+          </Switch>
+        </Router>
+
+
+
         <header className="App-header">
           <h1 className="App-title">Gallery of cats</h1>
         </header>
         <br />
-
         <Fragment className="gallery">
-
-          <GalleryList images={this.state.images} upvote={this.upvote}/> {/* hello */}
-
+          <GalleryList images={this.state.images} upvote={this.upvote} />
         </Fragment>
       </div>
     );
--- src/components/App/GalleryItem/GalleryItem.js
+++ src/components/App/GalleryItem/GalleryItem.js
@@ -5,17 +5,41 @@ class GalleryItem extends Component {
 
 
     render() {
+
+        
+
         return (
+
+
+
             <div className="row">
                 <div className="column">
                     <img src={this.props.image.path} alt={this.props.image.path} className="images" />
-                    <br />
-                    <button className="upvote" onClick={() => this.props.upvote(this.props.image.id)}>Upvote</button>
-                    <br />
-                    <p>{this.props.image.likes} upvotes</p>
+                    
+                    <div className="overlay">
+                        <div className="text">{this.props.image.description}</div>
+                    </div>
                 </div>
+                <br />
+                <button className="upvote" onClick={() => this.props.upvote(this.props.image.id)}>upvotes</button>
+                <p>{this.props.image.likes} upvotes</p>
+            </div >
 
-            </div>
+
+
+            // <div className="row">
+            //     <div className="column">
+            //         <img src={this.props.image.path} alt={this.props.image.path} className="images" />
+            //         <div className="overlay">
+            //             <div className="text">{this.props.image.description}</div>
+            //         </div>
+            //     </div>
+
+            //     <br />
+            //     <button className="upvote" onClick={() => this.props.upvote(this.props.image.id)}>Upvote</button>
+            //     <br />
+            //     <p>{this.props.image.likes} upvotes</p>
+            // </div>
         );
     }
 }
